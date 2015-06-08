d472a794a4722127e01dfc506c35fff2f7fd6429
Joshua Morton <joshua.morton@gatech.edu>
2015-06-07 23:25:07 -0400

    added updates to objectModel

--- objectModel.py
+++ objectModel.py
@@ -18,8 +18,6 @@ obj = dict()
 def string_function(instance):
     return "<instance of "+instance["_name"]+">"
 
-# here I sneak into python level code to allow us to differentiate between
-# things
 
 obj["_str"] = string_function
 
@@ -58,9 +56,50 @@ def new_object(clazz, *args):
         else:
             new_obj[value] = clazz[value]
 
-    new_obj["_init"]()
+    new_obj["_init"](*args)
     return new_obj
 
 
 x = new_object(obj)
 print x["_str"]()
+
+# now to deal with inheritence and an inheritant object model
+# we need a way to define a class in code, because building each class like we
+# did for obj would be a pain
+
+def new_class(parent, *instance_data, **methods):
+    new_cls = {val:parent[val] for val in parent}  # inheritence
+    new_cls["_parent_class"] = parent  # keep track of parent for reasons?
+    for value in instance_data:
+        new_cls[value] = None  # this isn't strictly necessary
+    for method in methods:
+        new_cls[method] = methods[method]  # this is though
+    return new_cls
+
+
+def init_person(instance, name, age):
+    instance["name"] = name
+    instance["age"] = age
+
+def person_string(instance):
+    return "<Hi my name is "+instance["name"]+">"
+
+def is_older(instance, other):
+    return instance["age"] > other["age"]
+
+
+Person = new_class(obj, "name", "age", _init=init_person, _str=person_string, is_older=is_older)
+
+john = new_object(Person, "john", 35)
+amy = new_object(Person, "amy", 26)
+
+print john["age"]
+print john["name"]
+print john["_str"]()
+print amy["is_older"](john)
+
+Child = new_class(Person, is_teen=lambda inst: 20 > inst["age"] > 12)
+
+billy = new_object(Child, "billy", 7)
+print billy["is_teen"]()
+print billy["age"]
